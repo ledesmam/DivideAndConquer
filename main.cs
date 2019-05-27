294f1ba86b45e8262eb759627c3f248f9afa2cf5
マイケル <maikeru@netzero.net>
2019-05-27 08:39:21 -0700

    Out Of Bounds Checking
    Add default playlist
    Check for s and p bounds
    Add Comments
    Add Playlist with less than 3 songs

--- main.cs
+++ main.cs
@@ -7,36 +7,51 @@ namespace ctu
 {
     static class Unit2
     {
+        static int count = 0;
         static int Main(string[] args)
         {
-            string n = args[0];
-            string[] a = { "She Runs", "She Jumps", "She leaps", "She Eats", "She sleeps", "Fun" };
+            string q = args[0];
+            //Set a Default Playlist
+            string[] a = { "Intro", "She Jumps", "She leaps", "She Eats", "She sleeps", "Outro" };
 
             if ( args.Length == 2 )
                 a = FillArray(args[1].Trim());
 
-
             Console.WriteLine(string.Format("Playlist: {0} ", string.Join(",", a)));
-            Console.WriteLine(string.Format("   Index: {0}", Search(a, n)));
+            Console.WriteLine(string.Format("   Query: {0}",q));
+            Console.WriteLine(string.Format("   Index: {0}", Search(a, q)));
+            Console.WriteLine(string.Format("  A Size: {0}\n   Count: {1}",a.Length, count));
 
             return 0;
         }
 
         public static int Search(string[] a, string q, int iter = 0, int p = 3)
         {
-            int s = a.Length / p;
-            if (iter > s) return -1;
+            // Check Bounds for p to length of array
+            if ( a.Length < p )
+                p = a.Length;
+            // Calculate the Max Iteration
+            int s = a.Length / p ;
 
+            // If Iteration Grater than Max then q not found
+            if (iter > s) 
+                return -1;
+
+            // Check for q in the p sublists
             for (int i = 0; i < p; i++)
             {
+                count ++;
+                // get the current index to compare to
                 int index = i * s + iter;
+                // Check for array bounds
                 if ( index > a.Length-1 )
                     return -1;
-                else if ( a[index].ToLower() == q.ToLower())
+                // Compare index contents to q
+                if ( a[index].ToLower() == q.ToLower())
                     return index;
 
             }
-
+            // Nothing found so Search at next iteration
             return Search(a, q, iter + 1, p);
         }
 
@@ -45,7 +60,6 @@ namespace ctu
 
         public static string[] FillArray(string f)
         {
-
             List<string> l = new List<string>();
             FileStream fs = new FileStream(@f, FileMode.Open, FileAccess.Read);
             using (StreamReader s = new StreamReader(fs))
@@ -54,9 +68,7 @@ namespace ctu
                 while ((txt = s.ReadLine()) != null)
                     l.Add(txt.Trim());
             }
-
             return l.ToArray(); ;
-
         }
     }
 }
